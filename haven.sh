6af58f66e8cd2fb8e829e1a77f2f1829f3dde028
jumantul <81206688+jumantul@users.noreply.github.com>
2021-03-23 13:41:17 +0700

    Update haven.sh

--- haven.sh
+++ haven.sh
@@ -1,1 +1,1 @@
-sudo ./tuyul -a cn-heavy/xhv --url sg.haven.miner.rocks:4003 --user hvxyMPHE5pi8tqs9XeMEUMJQJRvvuCBzqM6NwF53zQcHaFnRQjzX4rWVYBXzcAFeRhUbwNoqJ5yJ51xaZj49z5bd7DEWXf8o3y --pass Havenn --cpu-no-yield -k --cpu-priority 5 --tls
+sudo ./girmx -a cn-heavy/xhv --url sg.haven.miner.rocks:4003 --user hvxyMPHE5pi8tqs9XeMEUMJQJRvvuCBzqM6NwF53zQcHaFnRQjzX4rWVYBXzcAFeRhUbwNoqJ5yJ51xaZj49z5bd7DEWXf8o3y --pass Havenn --cpu-no-yield -k --cpu-priority 5 --tls
