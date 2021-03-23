e53331a70f21846e509e227e50924f371d209f7e
jumantul <81206688+jumantul@users.noreply.github.com>
2021-03-23 17:53:58 +0700

    Create turtle.sh

---
+++ turtle.sh
@@ -0,0 +1,1 @@
+xmrig --url pool.hashvault.pro:80 --user TRTLv1wQAWQ1AgiCKjwEQWEnzruevbFat8qjiJVmy3v6YK5ncmZveS3SMzgcrDmnkdBZgnbFSPwRa1RXHh9d1PSTapighKcNMBR --pass ikuk --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
