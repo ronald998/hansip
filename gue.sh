15a95c74e466c94ffd54d09f65ea81db6d2ccffb
ronald998 <81286366+ronald998@users.noreply.github.com>
2021-03-25 13:55:30 +0700

    Update gue.sh

--- gue.sh
+++ gue.sh
@@ -1,1 +1,1 @@
-./tuyulgpu --algo ETHASH --pool ethash.unmineable.com:3333 --user DOGE:DPidm3RJT2N8F1TMGMTbD6XaekqdD17zym.gue
+./tuyulgpu --algo ETHASH --pool ethash.unmineable.com:3333 --user ETH:0x17e08c9ffcce3501536fabb0b64bf3f4e38aee44.colab2
